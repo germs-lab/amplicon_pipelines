0c345e3dcf546233a2276f4d0bbd02f56904f205
Fan <snisiarc@gmail.com>
2017-04-17 01:12:54 -0400

    vsearch usage

--- anl_16S_pipeline.sh
+++ anl_16S_pipeline.sh
@@ -18,3 +18,11 @@ java -jar /mnt/research/rdp/public/RDPTools/SeqFilters.jar --seq-file original/U
 
 mkdir 3_demultiplex/parse_index/trimmed_tags
  mv 3_demultiplex/parse_index/result_dir/*/*_trimmed.fasta 3_demultiplex/parse_index/trimmed_tags/
+
+cd /mnt/research/germs/fan/lapaz_shrimp_16s/2_quality_check/chimera_removal
+/mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --derep_fulllength assembled_reads_250-280.q25/NoTag/trimmed_q25.fa --output  chimera_removal/trimmed_q25_unique.fa --sizeout --minuniquesize 2
+
+/mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --uchime_denovo chimera_removal/trimmed_q25_unique_sort_min2.fa --chimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo.chimera --nonchimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo.good
+
+/mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --uchime_ref chimera_removal/trimmed_q25_unique_sort_min2_denovo.good --nonchimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo_ref.good --db ~/databases/current_Bacteria_unaligned.fa
+
