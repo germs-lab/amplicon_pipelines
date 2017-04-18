d894dbd4665c2eb3b5cfc0871d3c273e02553fb3
Fan <snisiarc@gmail.com>
2017-04-18 09:47:50 -0400

    global mapping and submission info

--- anl_16S_pipeline.sh
+++ anl_16S_pipeline.sh
@@ -22,7 +22,11 @@ mkdir 3_demultiplex/parse_index/trimmed_tags
 cd /mnt/research/germs/fan/lapaz_shrimp_16s/2_quality_check/chimera_removal
 /mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --derep_fulllength assembled_reads_250-280.q25/NoTag/trimmed_q25.fa --output  chimera_removal/trimmed_q25_unique.fa --sizeout --minuniquesize 2
 
+cd /mnt/research/germs/fan/lapaz_shrimp_16s/2_quality_check
 /mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --uchime_denovo chimera_removal/trimmed_q25_unique_sort_min2.fa --chimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo.chimera --nonchimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo.good
 
+# submitted: 256G mem, used 2 hours for 420773 unique sequences.
 /mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --uchime_ref chimera_removal/trimmed_q25_unique_sort_min2_denovo.good --nonchimeras chimera_removal/trimmed_q25_unique_sort_min2_denovo_ref.good --db ~/databases/current_Bacteria_unaligned.fa
+
+/mnt/research/germs/softwares/vsearch-2.4.3-linux-x86_64/bin/vsearch --usearch_global assembled_reads_250-280.q25/NoTag/trimmed_q25.fa --db chimera_removal/trimmed_q25_unique_sort_min2_denovo_ref.good --id 0.985 --matched all_trimmed_chimed_finalized.fa
 
